019de2aa90bf8a086b408339c109f0f8f933aa47
arkaghosh <arkasparko@gmail.com>
2020-09-14 03:19:51 +0530

    added script for maintaining same header

---
+++ consistency.sh
@@ -0,0 +1,4 @@
+cat header.html index_raw.html > index.html
+cat header.html contact_raw.html > contact.html
+cat header.html research_interests_raw.html > research_interests.html
+cat header.html education_raw.html > education.html
